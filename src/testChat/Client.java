e70d65e4b11d5f543f9473796e52539feb3153c1
jnhsyxxy <32084540+jnhsyxxy@users.noreply.github.com>
2018-06-01 19:37:46 +0800

    Add files via upload

---
+++ src/testChat/Client.java
@@ -0,0 +1,312 @@
+package testChat;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Client {
+	
+	/**
+	 * @param chat_name:聊天的名字，若是群聊，则为群名；否则为对方好友的名字
+	 * @param isgroup:是否为群聊，true为是，false为否
+	 * @return 该聊天是否为当前聊天，true为是，false为否
+	 */
+	boolean isCurrentChat(String chat_name, boolean isgroup) {
+		//TODO 判断给定的聊天是否为当前主窗口中的聊天
+		return true;
+	}
+	/**
+	 * @param friend_name:发送信息者的用户名（可能是对方，也可能是自己）
+	 * @param self:发送信息者是否是自己，true为自己，false为不是
+	 * @param info:发送的信息
+	 */
+	void showMessage(String friend_name, boolean self, String info) {
+		//TODO 这个函数用来在当前界面中显示信息，对方的信息在左侧显示，自己的信息在右侧显示
+		
+	}
+	/**
+	 * @param chat_name:聊天的名字，若是群聊，则为群名；否则为对方好友的名字
+	 * @param isgroup:是否为群聊，true为是，false为否
+	 */
+	void remindMessage(String chat_name, boolean isgroup) {
+		//TODO 将指定的聊天（并非当前聊天）移动到好友列表的第一个，并显示一个红点
+		
+	}
+	
+	
+	
+	class RecieveThread extends Thread {
+		ObjectInputStream ois;
+
+		RecieveThread(ObjectInputStream ois) {
+			this.ois = ois;
+		}
+
+		public void run() {
+			while (true) {
+				Message msg = IOControl.read(ois);
+
+				int type = msg.get_type();
+				String from = msg.get_from();
+				String to = msg.get_to();
+				boolean isgroup = msg.get_isgroup();
+				String info = msg.get_msg();
+
+				if (type == 6) {
+					synchronized(requestFriend){
+						requestFriend.put(id, true);
+					}
+				}
+				if (type == 7) {
+				}
+				
+				if (type == 8) {
+					int fromid=Integer.parseInt(from);
+					if (id2name.containsKey(fromid))
+						System.out.println("("+id2name.get(fromid)+") "+info);
+					else
+						System.out.println("( id: "+fromid+" ) "+info);
+				}
+				
+				if (type == 9) {
+					parseFriend(info);
+				}
+				
+				if (type != 8)
+				System.out.println(
+						"type:" + type + ", from:" + from + ", to:" + to + ", isgroup:" + isgroup + ", msg:" + info);
+			}
+		}
+	}
+	
+	private String name;
+	private Integer id;
+	private ConcurrentHashMap<String, Integer> name2id;
+	private ConcurrentHashMap<Integer, String> id2name;
+	private HashMap<Integer, Boolean>requestFriend;
+
+	private void parse_information(String msg) {
+		msg.split("\n");
+	}
+
+	private Client() throws Exception {
+		OutputStream os = null;
+		PrintWriter pw = null;
+		ObjectOutputStream oos = null;
+		
+		boolean inChatRoom=false;
+		String defaultDes="";
+
+		InputStream is = null;
+		BufferedReader br = null;
+		ObjectInputStream ois = null;
+
+		Scanner sc = null;
+		Socket socket = null;
+		
+		name2id = new ConcurrentHashMap<String, Integer>();
+		id2name = new ConcurrentHashMap<Integer, String>();
+		requestFriend = new HashMap<Integer, Boolean>();
+
+		try {
+			String host = InetAddress.getLocalHost().getHostAddress();
+			System.out.println(host);
+			int port = 1234;
+
+			socket = new Socket(host, port);
+
+			os = socket.getOutputStream();
+			pw = new PrintWriter(os);
+			oos = new ObjectOutputStream(os);
+
+			is = socket.getInputStream();
+			br = new BufferedReader(new InputStreamReader(is));
+			ois = new ObjectInputStream(is);
+
+			sc = new Scanner(System.in);
+
+			boolean alreadySignIn = false;
+
+			while (alreadySignIn == false) {
+				int type = sc.nextInt();
+				sc.nextLine();
+
+				String tmp = null;
+				if (type != 3)
+					tmp = sc.nextLine();
+				if (can_send(type, 0, tmp))
+					IOControl.print(oos, new Message(type, tmp));
+				else
+					continue;
+
+				if (type == 3)
+					return;
+
+				Message msg = IOControl.read(ois);
+
+				type = msg.get_type();
+				String info = msg.get_msg();
+				System.out.println("(Server) " + info);
+				if (type == 4) {
+					alreadySignIn = true;
+					String[] user2passwd = tmp.split(" ");
+					name = user2passwd[0];
+					
+					parseInfo(info);
+				}
+			}
+			
+			RecieveThread recieve = new RecieveThread(ois);
+			recieve.start();
+			
+			boolean alreadySignOff = false;
+
+			while (alreadySignOff == false) {
+				
+				if (inChatRoom){
+					String inp=sc.nextLine();
+					if (inp.equals("\\quit"))
+					{
+						inChatRoom=false;
+						defaultDes="";
+					}else{
+						Message new_msg=new Message(8, id.toString(), defaultDes, false , inp);
+						IOControl.print(oos, new_msg);
+					}
+					continue;
+				}
+				
+				int type = sc.nextInt();
+				if (type==1001){
+					list_friend();
+					continue;
+				}
+				
+				if (type==3){
+					IOControl.print(oos, new Message(3, id.toString(), "0", false, ""));
+					alreadySignOff=false;
+				}
+				sc.nextLine();
+				
+				String to = sc.nextLine();
+				int toID=Integer.parseInt(to);
+				
+				String info = sc.nextLine();
+				Message new_msg = new Message(type, id.toString(), to, false, info);
+				if (can_send(type, toID, info))
+					IOControl.print(oos, new_msg);
+				
+				if (type==7){
+					IOControl.print(oos, new Message(9,id.toString(),"",false,""));
+				}
+				if (type==8){
+					inChatRoom=true;
+					defaultDes=to;
+					System.out.println("Start chat");
+				}
+			}
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (ois != null)
+				ois.close();
+			if (oos != null)
+				oos.close();
+			if (br != null)
+				br.close();
+			if (is != null)
+				is.close();
+			if (pw != null)
+				pw.close();
+			if (os != null)
+				os.close();
+			if (socket != null)
+				socket.close();
+			System.out.println("end!");
+		}
+	}
+	
+	private void parseInfo(String info) {
+		String tmp[] = info.split("\n");
+		parseFriend(tmp[0]);
+	}
+	
+	private void parseFriend(String info){
+		name2id.clear();
+		id2name.clear();
+		String fri[] = info.split(" ");
+		assert fri.length % 2 == 1;
+		id=Integer.parseInt(fri[0]);
+		for(int i=1;i<fri.length;i+=2) {
+			name2id.put(fri[i+1], Integer.parseInt(fri[i]));
+			id2name.put(Integer.parseInt(fri[i]), fri[i+1]);
+		}
+	}
+	
+	private boolean can_send(int type, int id, String msg){
+		switch(type){
+			case 1:
+			case 2:
+				int SpaceCount=0,nCount=0,len=msg.length();
+				for (int i=0;i<len;++i)
+				{
+					if (msg.charAt(i)==' ')
+						SpaceCount++;
+					if (msg.charAt(i)=='\n')
+						nCount++;
+				}
+				if (SpaceCount!=1||nCount>0)
+				{
+					System.out.println("no space and '\n' in account name or password!");
+					return false;
+				}
+				return true;
+			case 3:
+				return true;
+			case 6:
+				if (id2name.containsKey(id)){
+					System.out.println("you are already friends!");
+					return false;
+				}
+				return true;
+			case 7:
+				boolean result=false;
+				synchronized(requestFriend)
+				{
+					if (requestFriend.containsKey(id))
+					{
+						requestFriend.remove(id);
+						result=true;
+					}
+					else
+					{
+						System.out.println("Invalid accept packet!");
+					}
+				}
+				return result;
+			case 8:
+				if (id2name.containsKey(id)==false){
+					System.out.println("you are not friends!");
+					return false;
+				}
+				return true;
+			case 9:
+				return true;
+			default:
+				return false;
+		}
+	}
+	
+	private void list_friend(){
+		for (Map.Entry<String, Integer>e: name2id.entrySet())
+			System.out.println(e.getValue()+" "+e.getKey());
+	}
+
+	public static void main(String[] args) throws Exception {
+		Client myClient = new Client();
+	}
+}
---
+++ src/testChat/ServerThread.java
@@ -0,0 +1,287 @@
+package testChat;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+
+class BroadCastThread extends Thread{
+	Server mainServer = null;
+	Vector<String> to_member;
+	Message msg=null;
+	
+	public BroadCastThread(Server mainServer, Vector<String> to_member,Message msg){
+		this.mainServer=mainServer;
+		this.to_member=to_member;
+		this.msg=msg;
+	}
+	
+	public void run(){
+		for (String to : to_member){
+			ServerThread st=mainServer.get_accountServer(to);
+			if (st!=null)
+				st.write_to_account(msg);
+			else{
+				
+			}
+		}
+	}
+}
+
+public class ServerThread extends Thread {
+	private Server mainServer=null;
+	private Socket socket=null;
+	private String name=null;
+	
+	private ObjectOutputStream oos;
+	private ObjectInputStream ois;
+	
+	//private ArrayBlockingQueue<Message> queue=new ArrayBlockingQueue<Message>(100,false);
+	
+	public ServerThread(){}
+	
+	public ServerThread(Server mainServer, Socket socket){
+		this.mainServer=mainServer;
+		this.socket=socket;
+	}
+	
+	/*
+	 * 向负责的account发送msg包
+	 */
+	public void write_to_account(Message msg){
+		synchronized(oos){
+			IOControl.print(oos, msg);
+		}
+	}
+	
+	/*
+	 * 先验证登录确认身份，或者注册新用户.
+	 * 
+	 * 然后将该用户相关的可联系用户与群信息发过去
+	 * 
+	 * 之后通过双线程的IO保证
+	 */
+	@Override
+	public void run(){
+		InputStream is=null;
+		InputStreamReader isr=null;
+		BufferedReader br=null;
+		OutputStream os=null;
+		PrintWriter pw=null;
+		oos=null;
+		ois=null;
+		try {
+			os = socket.getOutputStream();
+			pw = new PrintWriter(os);
+			oos = new ObjectOutputStream(os);
+			
+			is=socket.getInputStream();
+			isr=new InputStreamReader(is);
+			br=new BufferedReader(isr);
+			ois=new ObjectInputStream(is);
+			
+			boolean alreadySignIn=false;
+			
+			while (alreadySignIn==false)
+			{
+				Message msg=IOControl.read(ois);
+				if (msg==null)
+				{
+					System.out.println("null class!");
+					return;
+				}
+				
+				int type=msg.get_type();
+				String info=null;
+				String[] tmp=null;
+				if (type!=3)
+				{
+					info=msg.get_msg();
+					tmp=info.split(" ");
+				}
+				
+				if (type==1){
+					String result=mainServer.sign_up(tmp[0], tmp[1]);
+					if (result!=null)
+						IOControl.print(oos,new Message(5,result));
+					else
+						IOControl.print(oos,new Message(5,"signupsuccess!"));
+				}
+				
+				if (type==2){
+					String result=mainServer.sign_in(tmp[0], tmp[1], this);
+					if (result!=null)
+						IOControl.print(oos,new Message(5,result));
+					else
+					{
+						alreadySignIn=true;
+						name=tmp[0];
+						IOControl.print(oos,new Message(4,
+							mainServer.get_account_message(tmp[0])));
+					}
+				}
+				
+				if (type==3)return;
+			}
+			
+			/*
+			 * 以下是sign in的情况
+			 * 几种操作：
+			 * ①添加好友，为ADD 好友名 信息
+			 * ②向好友发送信息，为 CHAT 好友名 信息
+			 * （目前测试直接在server加入好友信息
+			 * 
+			 * 但是因为需要同时处理收发,所以需要继续分化进程
+			 * 目前未做，因为没有？？
+			 */
+			
+			boolean alreadySignOff=false;
+			
+			while (alreadySignOff==false){
+				Message msg=IOControl.read(ois);
+				
+				if (msg==null)
+				{
+					System.out.println("Null Class");
+					return;
+				}
+				int type=msg.get_type();
+				String from=msg.get_from();
+				String to=msg.get_to();
+				boolean isgroup=msg.get_isgroup();
+				String info=msg.get_msg();
+				
+				Vector<String>to_member=new Vector<String>();
+				BroadCastThread broad=null;
+				
+				/*
+				 * 加好友请求
+				 * type==6
+				 */
+				if (type==6){
+					to_member.addElement(to);
+					broad = new BroadCastThread(mainServer, to_member, msg);
+					broad.start();
+				}
+				/*
+				 * 好友请求回复
+				 * type==7
+				 * msg 1:同意好友请求, 0:拒绝好友请求
+				 */
+				if (type==7){
+					if (info.equals("1")) {
+						mainServer.add_friend(Integer.parseInt(to), Integer.parseInt(from));
+					}
+					to_member.addElement(to);
+					broad = new BroadCastThread(mainServer, to_member, msg);
+					broad.start();
+				}
+				/*
+				 * 聊天信息包
+				 * type==6
+				 * from 信息来自的用户
+				 * to 信息去向的用户/群
+				 * msg 聊天内容
+				 */
+				if (type==8){
+					Message new_msg=null;
+					if(isgroup) {
+						to_member = mainServer.get_groupMember(to);
+						to_member.removeElement(from);
+						new_msg = new Message(type, from+"_"+to, to, isgroup, info);
+					}
+					else{
+						to_member.addElement(to);
+						new_msg=msg;
+					}
+					broad = new BroadCastThread(mainServer, to_member, new_msg);
+					broad.start();
+				}
+				/*
+				 * 请求好友列表
+				 * type==9
+				 */
+				if (type==9){
+					int id=Integer.parseInt(from);
+					String tmp=mainServer.get_friend(id),result=id+"";
+					if (tmp!=null)
+						result+=tmp;
+					Message new_msg=new Message(9,"",from,false,result);
+					to_member.addElement(from);
+					broad = new BroadCastThread(mainServer, to_member, new_msg);
+					broad.start();
+				}
+				/*
+				 * 删除好友
+				 * type==10
+				 */
+				if (type == 10) {
+					mainServer.delete_accountServer(Integer.parseInt(to));
+				}
+				/*
+				 * 创建群聊
+				 * type==11
+				 * from 群主
+				 * to 群成员1+群成员2+……
+				 * msg:群名称
+				 */
+				if(type == 11) {
+					String tmp[] = to.split("_");
+					for(String t : tmp) {
+						to_member.addElement(t);
+					}
+					//调用mainServer的创建群聊的方法
+					
+					broad = new BroadCastThread(mainServer, to_member, msg);
+					broad.start();
+				}
+				/*
+				 * 解散群聊
+				 * type==12
+				 * from 解散群聊的用户,必须是群主
+				 * to 解散的群聊
+				 */
+				if(type == 12) {
+					//调用mainServer的解散群聊的方法
+				}
+				/*
+				 * 邀请好友进群
+				 * type==13
+				 * from 发出邀请的用户
+				 * to 邀请的对象+邀请其加入的群
+				 */
+				if(type == 13) {
+					//调用mainServer的向群聊中加成员的方法
+				}
+				/*
+				 * 用户登出
+				 */
+				if (type==3){
+					alreadySignOff=true;
+				}
+			}
+			
+			sleep(10*1000);
+		}catch(IOException e){
+			System.out.println("IOException");
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}finally{
+			try{
+				if (oos!=null)oos.close();
+				if (ois!=null)ois.close();
+				if (pw!=null)pw.close();
+				if (os!=null)os.close();
+				if (br!=null)br.close();
+				if (isr!=null)isr.close();
+				if (is!=null)is.close();
+				if (socket!=null)socket.close();
+				System.out.println("socket end!");
+				mainServer.delete_accountServer(this.name);
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+		}
+	}
+}
